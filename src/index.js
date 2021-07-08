34000905365cc6afaa40077c2ba82837757b5a51
cjrequena828 <cjrequena827@gmail.com>
2021-07-08 10:48:19 -0400

    commit tree 72911d10f2113893ff998f2e287bae8717f21e49

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+//console.log("DOMContentLoaded");
 })
+ 
+const dogTable = document.getElementById("table-body");
+
+function fetch(){
+fetch(`http://localhost:3000/dogs`)
+ .then(response => response.json())
+ .then(console.log);
+ }
+
+// fetch("http://localhost:3000/dogs")
+// 		.then(resp => resp.json())
+// 		.then(console.log)
+
+function addDogToPage(dogs) {
+	console.log(addDogToPage);
+    dogs.forEach(function(dog) {
+		addDogTable(dog);
+	})
+}
+function addDogTable(dog){
+     newDogT = document.createElement('tr')
+     
+
+}
